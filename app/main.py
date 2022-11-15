391688fd41696130a9a1d0d315532eaf48e48e1f
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-11-15 20:24:20 +0300

    feat: admin can add luggage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from .models import Lauggage
 from . import db
 
 main = Blueprint('main', __name__)
@@ -11,4 +12,30 @@ def index():
 
 @main.route('/admin')
 def admin():
-    return render_template('admin.html')
+    luggages = Lauggage.query.all()
+    return render_template('admin.html', luggages=luggages)
+
+
+# @main.route('/admin')
+# def view_luggages():
+#     luggages = Lauggage.query.all()
+#     return render_template('admin.html', luggages=luggages)
+
+
+@main.route('/luggage')
+def luggage():
+    return render_template('luggage.html')
+
+
+@main.route('/luggage', methods=['POST'])
+def add_luggage():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    description = request.form.get('description')
+
+    new_luggage = Lauggage(email=email, name=name, description=description)
+
+    db.session.add(new_luggage)
+    db.session.commit()
+
+    return redirect(url_for('main.admin'))
--- app/models.py
+++ app/models.py
@@ -7,3 +7,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+
+class Lauggage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    description = db.Column(db.Text(350))
+
