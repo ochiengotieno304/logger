def91e0afecb96b8afc8b62467eae7b9ddcbc575
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-12-13 20:27:18 +0300

    feat: added sms

--- app/main.py
+++ app/main.py
@@ -2,9 +2,18 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from .models import Lauggage
 from . import db
+import africastalking
+import os
 
 
 main = Blueprint('main', __name__)
+
+username = os.getenv('user_name', 'sandbox')
+api_key = os.getenv('api_key')
+phone = os.getenv('phone')
+
+africastalking.initialize(username, api_key)
+sms = africastalking.SMS
 
 
 @main.route('/')
@@ -32,7 +41,14 @@ def add_luggage():
     db.session.add(new_luggage)
     db.session.commit()
 
+    sms.send(f"Dear {new_luggage.name} your luggage ID: 00{new_luggage.id} has been logged into our storage", [f"{phone}"], callback=on_finish)
     return redirect(url_for('main.index'))
+
+
+def on_finish(error, response):
+    if error is not None:
+        raise error
+    print(response)
 
 
 @main.route('/luggage/<id>')
@@ -41,3 +57,20 @@ def view_luggage(id):
     luggage = Lauggage.query.filter_by(id=id).first_or_404()
     data = f"Luggage ID: {luggage.id} Owner: {luggage.name} Contact: {luggage.email}"
     return render_template('show_luggage.html', luggage=luggage, data=data)
+
+
+@main.route('/logout/<id>')
+@login_required
+def logout(id):
+    luggage = Lauggage.query.filter_by(id=id).first_or_404()
+
+    sms.send(f"Dear {luggage.name} your luggage ID: 00{luggage.id} has been logged out of our storage", [f"{phone}"], callback=on_finish)
+
+    db.session.delete(luggage)
+    db.session.commit()
+    flash("Luggage logged out")
+
+    return redirect(url_for('main.index'))
+
+
+
